815976a97a436435c2387905ec96cb18ffabb9ed
Staehler, Michael <michael.staehler@opitz-consulting.com>
2017-08-25 16:45:34 +0200

    added sample route

---
+++ spring-boot-camel-soap-client/src/main/java/de/fred4jupiter/springbootcamelsoap/CountryCaller.java
@@ -0,0 +1,25 @@
+package de.fred4jupiter.springbootcamelsoap;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import countries.wsdl.Country;
+import countries.wsdl.GetCountryResponse;
+
+@Component
+public class CountryCaller implements Processor {
+
+	@Autowired
+	private CountriesClient countriesClient;
+
+	@Override
+	public void process(Exchange exchange) throws Exception {
+		String countryName = exchange.getIn().getBody(String.class);
+		GetCountryResponse response = countriesClient.getCountry(countryName);
+		Country country = response.getCountry();
+		exchange.getIn().setBody(country.getCapital(), String.class);
+	}
+
+}
--- spring-boot-camel-soap-client/src/main/java/de/fred4jupiter/springbootcamelsoap/MyCamelRouteBuilder.java
+++ spring-boot-camel-soap-client/src/main/java/de/fred4jupiter/springbootcamelsoap/MyCamelRouteBuilder.java
@@ -8,6 +8,7 @@ public class MyCamelRouteBuilder extends SpringRouteBuilder {
 
 	@Override
 	public void configure() throws Exception {
+		from("direct:countries").bean("countryCaller").to("mock:end");
 	}
 
 }
