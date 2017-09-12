ea7b145b23eabbd8e55f18523d0fa033675bd39a
Staehler, Michael <michael.staehler@opitz-consulting.com>
2017-09-12 16:14:15 +0200

    added simple test

---
+++ plain-camel/src/test/java/de/fred4jupiter/plaincamel/ProcessFileTest.java
@@ -0,0 +1,28 @@
+package de.fred4jupiter.plaincamel;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+
+import java.io.File;
+
+public class ProcessFileTest extends CamelTestSupport {
+
+	@Override
+	protected RouteBuilder createRouteBuilder() throws Exception {
+		return new MyRoute();
+	}
+
+	@Test
+	public void moveFileAndCheckContent() throws Exception {
+		template.sendBodyAndHeader("file://target/in", "Hello World", Exchange.FILE_NAME, "hello.txt");
+		Thread.sleep(2000);
+		File target = new File("target/out/hello.txt");
+		assertTrue("File not moved", target.exists());
+
+		String fileContent = FileUtils.readFileToString(target);
+		assertEquals("Hello World, filename: hello.txt", fileContent);
+	}
+}
