f17a8adbb95ef851a94a7cda75c06b815fe6de74
fred4jupiter <hamsterhase@gmx.de>
2017-09-12 15:46:44 +0200

    update route

--- plain-camel/src/main/java/de/fred4jupiter/plaincamel/MyRoute.java
+++ plain-camel/src/main/java/de/fred4jupiter/plaincamel/MyRoute.java
@@ -9,7 +9,7 @@ public class MyRoute extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("file://target/in?autoCreate=true&move=.done").log("This is the body: ${body}").to("direct:inbox");
+        from("file://target/in?autoCreate=true").log("This is the body: ${body}").to("direct:inbox");
 
         from("direct:inbox").bean(BodyProcessor.class).to("file://target/out");
     }
