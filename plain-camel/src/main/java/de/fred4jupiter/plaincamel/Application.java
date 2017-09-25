423bddcec5109d7783666450a43d121788c5b02b
Lack, Stefan <Stefan.Lack@opitz-consulting.com>
2017-09-25 11:45:15 +0200

    Use org.apache.camel.main.Main for sample application

--- plain-camel/src/main/java/de/fred4jupiter/plaincamel/Application.java
+++ plain-camel/src/main/java/de/fred4jupiter/plaincamel/Application.java
@@ -1,20 +1,28 @@
 package de.fred4jupiter.plaincamel;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.main.Main;
 
 /**
  * Created by michael on 12.09.2017.
  */
-public class Application {
+public class Application extends org.apache.camel.main.Main {
+
+	public Application() throws Exception {
+		super();
+		getOrCreateCamelContext().addRoutes(new MyRouteBuilder());
+	}
+
+	@Override
+	protected CamelContext createContext() {
+		CamelContext camelContext = CamelContextCreator.createCamelContext();
+		return camelContext;
+	}
 
 	public static void main(String[] args) throws Exception {
-		CamelContext camelContext = CamelContextCreator.createCamelContext();
 
-		camelContext.addRoutes(new MyRouteBuilder());
+		Application application = new Application();
+		application.run();
 
-		camelContext.start();
-		while (Thread.activeCount() > 1) {
-			// blocking main thread
-		}
 	}
 }
