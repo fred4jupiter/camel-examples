be3d91d8001f6e2ac03c0e90f31be31e7394ad1e
Staehler, Michael <michael.staehler@opitz-consulting.com>
2017-09-27 09:27:04 +0200

    simplified

--- plain-camel/src/main/java/de/fred4jupiter/plaincamel/BodyProcessor.java
+++ plain-camel/src/main/java/de/fred4jupiter/plaincamel/BodyProcessor.java
@@ -1,9 +1,9 @@
 package de.fred4jupiter.plaincamel;
 
 import org.apache.camel.Body;
-import org.apache.camel.Exchange;
 import org.apache.camel.Handler;
 import org.apache.camel.Header;
+import org.apache.camel.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,13 +12,13 @@ import org.slf4j.LoggerFactory;
  */
 public class BodyProcessor {
 
-    private static final Logger LOG = LoggerFactory.getLogger(BodyProcessor.class);
+	private static final Logger LOG = LoggerFactory.getLogger(BodyProcessor.class);
 
-    @Handler
-    public void enrichBody(@Header("CamelFileName") String fileName, @Body String body, Exchange exchange) {
-        LOG.debug("filename: {}", fileName);
+	@Handler
+	public void enrichBody(@Header("CamelFileName") String fileName, @Body String body, Message message) {
+		LOG.debug("filename: {}", fileName);
 
-        String newBody = body + ", filename: "+fileName;
-        exchange.getIn().setBody(newBody);
-    }
+		String newBody = body + ", filename: " + fileName;
+		message.setBody(newBody);
+	}
 }
